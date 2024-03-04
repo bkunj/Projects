4133dbf1083544c12f36cb2e3ffc36846e8ae537
bkunj <kunjb6789@gmail.com>
2024-03-04 18:24:37 +0530

    first commit

---
+++ ToDo-App-React/src/components/TodoItem1.jsx
@@ -0,0 +1,23 @@
+function TodoItem1() {
+
+    let todoName = 'Buy Milk';
+    let todoDate = '4/10/2023';
+
+  return (
+    <>
+    <div class="container">
+      <div class="row kb-row">
+        <div class="col-6">{todoName}</div>
+        <div class="col-4">{todoDate}</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger  kb-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default TodoItem1;
